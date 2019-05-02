1595cb0727b6fa140c3165a84eb5be18aecc988a
VladimirTsibrov <39917606+VladimirTsibrov@users.noreply.github.com>
2019-05-02 12:31:20 +0300

    Update inputValBitwise.ino

--- examples/inputValBitwise/inputValBitwise.ino
+++ examples/inputValBitwise/inputValBitwise.ino
@@ -1,6 +1,8 @@
 #include <Wire.h>
 #include <LiquidCrystal_I2C_Menu.h>
-LiquidCrystal_I2C_Menu lcd(0x27, 20, 4);
+#define cols 20
+#define rows 4
+LiquidCrystal_I2C_Menu lcd(0x27, cols, rows);
 
 // Encoder pins
 #define pinCLK 2
@@ -15,11 +17,15 @@ void setup() {
 }
 
 void loop() {
-  if (lcd.inputValBitwise("Input value", val, 5, 2, 1)) // Asking for [-]XXX.XX value
-    lcd.printf("You entered %Lf", val);
+  if (lcd.inputValBitwise("Input value", val, 5, 2, 1)) { // Asking for [-]XXX.XX value
+    lcd.print("You entered: ");
+    lcd.print(val);
+  }
   else
     lcd.print("Input canceled");
-  lcd.printAt(0, 2, "Press button");
-  lcd.printAt(0, 3, "to continue");
+  if (rows == 4) {
+    lcd.printAt(0, 2, "Press button");
+    lcd.printAt(0, 3, "to continue");
+  }
   while (lcd.getEncoderState() != eButton);
 }
